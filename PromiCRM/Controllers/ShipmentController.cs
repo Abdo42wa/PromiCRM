d0752747e9c5a22e9c9c71e2db98d5cb747e94ae
abdo42wa <abdelrahman-ahmed-ali-mohamed.elsayed@stud.viko.lt>
2021-11-02 21:00:14 +0200

    Shipment Controller

--- PromiCRM/Controllers/ShipmentController.cs
+++ PromiCRM/Controllers/ShipmentController.cs
@@ -14,13 +14,13 @@ namespace PromiCRM.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ServiceController : ControllerBase
+    public class ShipmentController : ControllerBase
     {
         private readonly IUnitOfWork _unitOfWork;
         private readonly IMapper _mapper;
-        private readonly ILogger<ServiceController> _logger;
+        private readonly ILogger<ShipmentController> _logger;
 
-        public ServiceController(IUnitOfWork unitOfWork, IMapper mapper, ILogger<ServiceController> logger)
+        public ShipmentController(IUnitOfWork unitOfWork, IMapper mapper, ILogger<ShipmentController> logger)
         {
             _unitOfWork = unitOfWork;
             _mapper = mapper;
@@ -31,21 +31,21 @@ namespace PromiCRM.Controllers
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        public async Task<IActionResult> GetServices()
+        public async Task<IActionResult> GetShipments()
         {
-            var services = await _unitOfWork.Services.GetAll();
-            var results = _mapper.Map<IList<ServiceController>>(services);
+            var shipment = await _unitOfWork.Shipments.GetAll();
+            var results = _mapper.Map<IList<ShipmentController>>(shipment);
             return Ok(results);
         }
 
 
-        [HttpGet("{id:int}", Name = "GetService")]
+        [HttpGet("{id:int}", Name = "GetShipment")]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        public async Task<IActionResult> GetService(int id)
+        public async Task<IActionResult> GetShipment(int id)
         {
-            var service = await _unitOfWork.Services.Get(c => c.Id == id);
-            var result = _mapper.Map<ServiceDTO>(service);
+            var shipment = await _unitOfWork.Shipments.Get(c => c.Id == id);
+            var result = _mapper.Map<ShipmentDTO>(shipment);
             return Ok(result);
         }
 
@@ -54,18 +54,18 @@ namespace PromiCRM.Controllers
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<IActionResult> CreateService([FromBody] CreateServiceDTO serviceDTO)
+        public async Task<IActionResult> CreateShipment([FromBody] CreateShipmentDTO shipmentDTO)
         {
             if (!ModelState.IsValid)
             {
-                _logger.LogError($"Invalid CREATE attempt in {nameof(CreateSeervice)}");
+                _logger.LogError($"Invalid CREATE attempt in {nameof(CreateShipment)}");
                 return BadRequest("Submited invalid data");
             }
-            var service = _mapper.Map<Service>(serviceDTO);
-            await _unitOfWork.Services.Insert(service);
+            var shipment = _mapper.Map<Shipment>(shipmentDTO);
+            await _unitOfWork.Shipments.Insert(shipment);
             await _unitOfWork.Save();
 
-            return CreatedAtRoute("GetService", new { id = service.Id }, service);
+            return CreatedAtRoute("GetShipment", new { id = shipment.Id }, shipment);
         }
 
 
@@ -73,23 +73,23 @@ namespace PromiCRM.Controllers
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<IActionResult> UpdateService([FromBody] UpdateServiceDTO serviceDTO, int id)
+        public async Task<IActionResult> UpdateShipment([FromBody] UpdateShipmentDTO shipmentDTO, int id)
         {
             if (!ModelState.IsValid)
             {
-                _logger.LogError($"Invalid UPDATE attempt in {nameof(UpdateService)}");
+                _logger.LogError($"Invalid UPDATE attempt in {nameof(UpdateShipment)}");
                 return BadRequest("Submited invalid data");
             }
-            var service = await _unitOfWork.Services.Get(c => c.Id == id);
-            if (service == null)
+            var shipment = await _unitOfWork.Shipments.Get(c => c.Id == id);
+            if (shipment == null)
             {
-                _logger.LogError($"Invalid UPDATE attempt in {nameof(UpdateService)}");
+                _logger.LogError($"Invalid UPDATE attempt in {nameof(UpdateShipment)}");
                 return BadRequest("Submited invalid data");
             }
 
 
-            _mapper.Map(serviceDTO, service);
-            _unitOfWork.Services.Update(service);
+            _mapper.Map(shipmentDTO, shipment);
+            _unitOfWork.Shipments.Update(shipment);
             await _unitOfWork.Save();
             return NoContent();
         }
@@ -99,15 +99,15 @@ namespace PromiCRM.Controllers
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<IActionResult> DeleteService(int id)
+        public async Task<IActionResult> DeleteShipment(int id)
         {
-            var service = _unitOfWork.Services.Get(c => c.Id == id);
-            if (service == null)
+            var shipment = _unitOfWork.Shipments.Get(c => c.Id == id);
+            if (shipment == null)
             {
-                _logger.LogError($"Invalid DELETE attempt in {nameof(DeleteService)}");
+                _logger.LogError($"Invalid DELETE attempt in {nameof(DeleteShipment)}");
                 return BadRequest("Submited invalid data");
             }
-            await _unitOfWork.Services.Delete(id);
+            await _unitOfWork.Shipments.Delete(id);
             await _unitOfWork.Save();
             return NoContent();
         }
