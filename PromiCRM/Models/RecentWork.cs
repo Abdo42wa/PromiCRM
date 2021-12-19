d69a012213e579b04c85be083bf3e6759c9eafa2
songulija <65647664+songulija@users.noreply.github.com>
2021-12-19 18:09:30 +0200

    Added RecentWork and RecentWorkDTO

---
+++ PromiCRM/Models/RecentWork.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PromiCRM.Models
+{
+    public class RecentWork
+    {
+        public int Id { get; set; }
+        [ForeignKey(nameof(Product))]
+        public int ProductId { get; set; }
+        public virtual Product Product { get; set; }
+        public int Quantity { get; set; }
+        [ForeignKey(nameof(User))]
+        public int UserId {get;set; }
+        public virtual User User { get; set; }
+        public string WorkTitle { get; set; }
+    }
+}
---
+++ PromiCRM/ModelsDTO/RecentWorkDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PromiCRM.ModelsDTO
+{
+    public class CreateRecentWorkDTO
+    {
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public int UserId { get; set; }
+        public string WorkTitle { get; set; }
+    }
+    public class UpdateRecentWorkDTO : CreateRecentWorkDTO
+    {
+    }
+    public class RecentWorkDTO : CreateRecentWorkDTO
+    {
+        public int Id { get; set; }
+        public ProductDTO Product { get; set; }
+        public UserDTO User { get; set; }
+    }
+}
