4bc27075e592f78c343fa842fa2789c1dfd1e998
songulija <65647664+songulija@users.noreply.github.com>
2021-12-26 18:05:39 +0200

    Changed orderController

--- PromiCRM/ModelsDTO/OrderDTO.cs
+++ PromiCRM/ModelsDTO/OrderDTO.cs
@@ -47,6 +47,7 @@ namespace PromiCRM.ModelsDTO
     {
         public int Id { get; set; }
         public string ImagePath { get; set; }
+        public int WeekNumber { get; set; }
         public UserDTO User { get; set; }
         public CustomerDTO Customer { get; set; }
         public ShipmentDTO Shipment { get; set; }
